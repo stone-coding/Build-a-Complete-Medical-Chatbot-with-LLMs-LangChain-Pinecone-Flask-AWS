58a51c923436d417fcfe5d1030f182be7c589db4
Haomiao Shi <stone.haomiao.shi@gmail.com>
2025-08-11 23:18:32 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Haomiao Shi",
+    author_email="stone.haomiao.shi@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
